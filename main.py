c0dd9bfccff13c773aca4150b7f96a56329628da
OleksandrHub <sasha.klishch1@gmail.com>
2025-02-16 22:05:53 +0200

    feat(R2Vector): improve R2Vector class
    - Added __repr__ method for better output
    - Added __add__ method for vector addition
    - Improved __str__ method for printing
    - Added R3Vector class inheriting from R2Vector
    - Updated print statements for clarity

--- main.py
+++ main.py
@@ -7,7 +7,25 @@ class R2Vector:
     def norm(self):
         return sum(val**2 for val in vars(self).values())**0.5
     def __str__(self):
-        return (getattr(self, i) for i in vars(self))
+        return str(tuple(getattr(self, i) for i in vars(self)))
+    
+    def __repr__(self):
+        arg_list = [f'{key}={val}' for key, val in vars(self).items()]
+        args = ", ".join(arg_list)
+        return f'{self.__class__.__name__}({args})'
+    
+    def __add__(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        kwargs = {i: getattr(self, i) + getattr(other, i) for i in vars(self)}
+        return self.__class__(**kwargs)
+    
+    def __sub__(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        kwargs = {i: getattr(self, i) + getattr(other, i) for i in vars(self)}
+        return self.__class__(**kwargs)
+    
 class R3Vector(R2Vector):
     def __init__(self, *, x, y, z):
         super().__init__(x=x, y=y)
@@ -15,5 +33,5 @@ class R3Vector(R2Vector):
 
 v1 = R2Vector(x=2, y=3)
 v2 = R3Vector(x=2, y=2, z=3)
-print(v1.norm())
-print(v2.norm())
+print(f'v1 = {v1}')
+print(f'v2 = {v2}')
